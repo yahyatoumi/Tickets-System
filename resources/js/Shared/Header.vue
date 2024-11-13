f38bd5ba0c07d4a3c3bd6ebf14c36d1cb2e2604d
unknown <emdrtad@emds-f010.cld.com>
2024-11-13 16:30:57 +0100

    responsive side bar menu

--- resources/js/Shared/Header.vue
+++ resources/js/Shared/Header.vue
@@ -2,9 +2,18 @@
     <header class="md:flex md:shrink-0">
         <div class="flex items-center justify-between px-6 py-4 bg-indigo-900 md:shrink-0 md:justify-center md:w-56">
             <div>logo</div>
-            <button class="md:hidden" type="button">
-                <v-icon name="fc-menu" class="w-8 h-8" />
-            </button>
+            <dropdown class="md:hidden" placement="bottom-end">
+                <template #default>
+                    <svg class="w-6 h-6 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                    </svg>
+                </template>
+                <template #dropdown>
+                    <div class="mt-2 px-8 py-4 bg-indigo-800 rounded shadow-lg">
+                        <side-bar-nav />
+                    </div>
+                </template>
+            </dropdown>
         </div>
         <div class="md:text-md flex items-center justify-between p-4 w-full text-sm bg-white border-b md:px-12 md:py-0">
             <div></div>
@@ -49,8 +58,7 @@
                         </span>
                         <v-icon name="io-ticket" class="fill-gray-400 group-hover:fill-white" />
                         </Link>
-                        <Link 
-                            class="group px-6 py-2 hover:text-white hover:bg-indigo-500 flex items-center gap-2"
+                        <Link class="group px-6 py-2 hover:text-white hover:bg-indigo-500 flex items-center gap-2"
                             href="/chat">
                         <span>
                             Chat
@@ -69,19 +77,21 @@
 <script>
 import Dropdown from '@/Shared/Dropdown.vue'
 import { isEndUser } from '@/helpers/rolesHelpers';
+import SideBarNav from './SideBarNav.vue';
 
 export default {
     components: {
         Dropdown,
+        SideBarNav
     },
     computed: {
         auth() {
             return this.$page.props.auth
         }
     },
-    methods:{
+    methods: {
         isEndUser
     }
-    
+
 }
 </script>
