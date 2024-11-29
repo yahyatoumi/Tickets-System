397664d10968f342ec106cd37badbe28a235eca8
yahyatoumi <yahyatoumi555@gmail.com>
2024-11-29 10:44:22 +0100

    ssl with nginx

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,18 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    server: {
-        hmr: {
-            host: "127.0.0.1",
-        },
-        port: 3000,
-        host: true,
-    },
+    // server: {
+    //     hmr: {
+    //         host: "127.0.0.1",
+    //     },
+    //     port: 3000,
+    //     host: true,
+    // },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-            // buildDirectory: '/', // This ensures the manifest is generated in the .vite directory
+            buildDirectory: 'public/build', // This ensures the manifest is generated in the .vite directory
         }),
         // Add this vue plugin configuration here
         vue({
@@ -26,10 +26,10 @@ export default defineConfig({
             },
         })
     ],
-    // build: {
-    //     outDir: 'public/build', // Ensure assets are built in the public directory
-    //     manifest: true,         // Laravel needs this to load assets
-    // },
+    build: {
+        outDir: 'public/build', // Ensure assets are built in the public directory
+        manifest: true,         // Laravel needs this to load assets
+    },
     resolve: {
         alias: {
             '@': '/resources/js',
