a7d908d4d2800c7091371829e4e19e9e9d2dfffe
yahyatoumi <yahyatoumi555@gmail.com>
2024-12-06 10:47:35 +0100

    build manual chunks

--- vite.config.js
+++ vite.config.js
@@ -31,4 +31,15 @@ export default defineConfig({
             '@': '/resources/js',
         },
     },
+    build: {
+        rollupOptions: {
+            output:{
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    }
 });
